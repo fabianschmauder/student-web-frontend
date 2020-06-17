2cec8cff619ed616a8a4ca843cc2bcb1ffecc1b9
fabianschmauder <fabianschmauder@gmail.com>
2020-06-17 13:08:44 +0200

    Add students fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+function loadStudents() {
+  return fetch("http://localhost:8080/students")
+      .then(response => response.json())
+}
 
 function App() {
+
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    loadStudents().then(data => setStudents(data));
+  },[]);
+
   return (
     <div>
-
+      {students.map((student) => <div key={student.id}>{student.name}</div>)}
     </div>
   );
 }
